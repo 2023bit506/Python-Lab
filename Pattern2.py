0c9759163a1f410069fa9bbd49c102a8ee890acc
Shubham Pradip Pawar <2023bit506@sggs.ac.in>
2024-08-15 17:41:32 +0530

    Pattern2.py

--- Pattern2.py
+++ Pattern2.py
@@ -1,21 +1,21 @@
 #only last line is -
 def Q1(l):
-	len = l*2+1
-	for i in range(0,(len//2)+1):
-		for j in range(0,len):
-			if j==(len//2)-i:
+	length = l*2+1
+	for i in range(0,(length//2)+1):
+		for j in range(0,length):
+			if j==(length//2)-i:
 				print("+",end="")
-			elif j==(len//2)+i:
+			elif j==(length//2)+i:
 				print("+",end="")	
 			else:
 				print(" ",end="")
 		print("")
 		
-	for i in range(0,(len//2)-1):
-		for j in range(0,len):
+	for i in range(0,(length//2)-1):
+		for j in range(0,length):
 			if j==i+1:
 				print("+",end="")
-			elif j==(len-1)-i-1:
+			elif j==(length-1)-i-1:
 				print("+",end="")
 				break	
 			else:
@@ -23,8 +23,8 @@ def Q1(l):
 			
 		print("")
 	for i in range(1):
-		for j in range(0,len):
-			if(j==len//2):
+		for j in range(0,length):
+			if(j==length//2):
 				print("-",end="")
 			else:
 				print(" ",end="")
@@ -33,22 +33,22 @@ def Q1(l):
 	return ""
 #lower tringle is -
 def Q2(l):
-	len = l*2+1
-	for i in range(0,(len//2)+1):
-		for j in range(0,len):
-			if j==(len//2)-i:
+	length = l*2+1
+	for i in range(0,(length//2)+1):
+		for j in range(0,length):
+			if j==(length//2)-i:
 				print("+",end="")
-			elif j==(len//2)+i:
+			elif j==(length//2)+i:
 				print("+",end="")	
 			else:
 				print(" ",end="")
 		print("")
 		
-	for i in range(0,(len//2)-1):
-		for j in range(0,len):
+	for i in range(0,(length//2)-1):
+		for j in range(0,length):
 			if j==i+1:
 				print("-",end="")
-			elif j==(len-1)-i-1:
+			elif j==(length-1)-i-1:
 				print("-",end="")
 				break	
 			else:
@@ -56,8 +56,8 @@ def Q2(l):
 			
 		print("")
 	for i in range(1):
-		for j in range(0,len):
-			if(j==len//2):
+		for j in range(0,length):
+			if(j==length//2):
 				print("-",end="")
 			else:
 				print(" ",end="")
@@ -65,15 +65,15 @@ def Q2(l):
 
 	return ""
 #main program-->
-def print_pattern(len):
-    if len<1:
+def print_pattern(length):
+    if length<1:
         return "Enter a number greater than equals to 1"
-    if len%1!=0:
+    if length%1!=0:
         return "Provide positive integer value greater than equal to 1"
     else:
-        return Q1(len) 
+        return Q1(length) 
 	
 
 
-print(Q1(2.00))
+print(Q1(2))
 # print(Q2(2))
